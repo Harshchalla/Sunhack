7a3a0745fd19bb5e042011a2e8e258d30e829103
Rohan Awhad <rohanavad007@gmail.com>
2024-09-28 22:25:03 -0700

    caching distinct faces

--- backend/main.py
+++ backend/main.py
@@ -16,24 +16,23 @@ def calculate_video_hash(video_file):
 
 
 
-def save_video_tmp(video_file):
-    sess_id = str(uuid.uuid4())
-    tmp_folder = f"/tmp/{sess_id}"
+def save_video_tmp(video_file, video_hash):
+    tmp_folder = f"/tmp/{video_hash}"
     os.makedirs(tmp_folder, exist_ok=True)
     video_filepath = os.path.join(tmp_folder, video_file.name)
     with open(video_filepath, "wb") as f:
         f.write(video_file.read())
-    return video_filepath, sess_id
+    return video_filepath
 
 
 def process_video(video_file):
     """Process the video and cache the result"""
     video_hash = calculate_video_hash(video_file)
     if f"faces_zipfilepath_{video_hash}" not in st.session_state:
-        video_filepath, sess_id = save_video_tmp(video_file)
-        faces_zipfilepath = distinct_faces.main(video_filepath, sess_id)
+        video_filepath = save_video_tmp(video_file)
+        faces_zipfilepath = distinct_faces.main(video_filepath, video_hash)
         st.session_state[f"faces_zipfilepath_{video_hash}"] = faces_zipfilepath
-    return st.session_state[f"faces_zipfilepath_{video_hash}"], sess_id
+    return st.session_state[f"faces_zipfilepath_{video_hash}"], video_hash
 
 # ===
 # Streamlit
@@ -42,15 +41,17 @@ st.title("Video Upload App")
 video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
 
 if video_file is not None:
-    faces_zipfilepath, sess_id = process_video(video_file)
+    faces_zipfilepath, video_hash = process_video(video_file)
     captions_list: list[str] = []
     with ZipFile(faces_zipfilepath, 'r') as zip_ref:
         for face_filename in zip_ref.namelist():
             if face_filename.endswith(".png"):
-                caption = face_filename[len(sess_id)+1:]
+                caption = face_filename[len(video_hash)+1:]
                 face_bytes = zip_ref.read(face_filename)
                 face_bytes_io = BytesIO(face_bytes)
                 st.image(face_bytes_io, caption=caption, width=200)
                 captions_list.append(caption)
     selected_captions = st.multiselect("Select the distinct faces", captions_list)
-
+    # add a submit button
+    if st.button("Submit"):
+        pass
